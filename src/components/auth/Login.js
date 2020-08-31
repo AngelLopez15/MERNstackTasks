e3a3b487feb01a70eb50c2d2a8201fc9406e2e5d
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-08-31 15:12:04 -0500

    Agregue Login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,50 @@
 import React from 'react'
 
 export const Login = () => {
+
+  const onChange = () =>{
+
+  }
+
   return (
-    <h1>Desde Login</h1>
+    <div className="form-usuario">
+
+      <div className="contenedor-form sombra-dark">
+        <h1>Inicio</h1>
+        <form>
+
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input 
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Correo"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input 
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Contraseña"
+              onChange={onChange}
+            />
+          </div>
+
+          <div>
+            <input 
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Iniciar sesión"
+            />
+          </div>
+
+        </form>
+      </div>
+    </div>
   )
 }
